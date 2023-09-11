743e44cf1abc9992e26cbc520a8cbe6984d083fa
Kiran-Kumar11 <144537776+Kiran-Kumar11@users.noreply.github.com>
2023-09-11 08:59:35 +0000

    date and time

--- lectures/script.js
+++ lectures/script.js
@@ -1,1 +1,19 @@
-console.log("kiran")
+// let todayDate = new Date();
+// console.log(todayDate.toLocaleString())
+// console.log(todayDate.toDateString());
+// console.log(todayDate.toLocaleDateString());
+
+// console.log(typeof todayDate)
+
+// let myDate = new Date("2023-02-17")
+// console.log(myDate.toLocaleString())
+
+// let anotherDate = new Date(2023,3,20, 5,30)
+// console.log(anotherDate.toLocaleString())
+
+// let otherDate = new Date("02-12-2023")
+// console.log(otherDate.toLocaleString());
+
+
+let date = new Date()
+console.log(date)
