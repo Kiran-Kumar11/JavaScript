55ab3062e03b8bebeb65ed6a2de6a290dce327f4
Kiran-Kumar11 <144537776+Kiran-Kumar11@users.noreply.github.com>
2023-09-14 12:15:29 +0000

    arrays

---
+++ lectures/arrays.js
@@ -0,0 +1,20 @@
+const myArray = [0,1,2,3,4,5]
+// console.log(myArray.push(6))
+
+// const fruits =["apple","banana","mango"]
+// const modifiedFruits = fruits.join("-");
+// console.log(modifiedFruits);
+
+// const newFruits =fruits.join(" ")
+// console.log(newFruits);
+
+// const freshFruits = fruits.join(",")
+// console.log(freshFruits);
+
+// const newArray = myArray.slice(1,3)
+// console.log(newArray)
+
+// const anotherArray = myArray.splice(1,3)
+// console.log(anotherArray)
+const vegetables = ["tomato","ladies finger"]
+
