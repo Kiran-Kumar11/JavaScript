25ec16740f462718a033dd0e8f6b12ca329dfa67
Kiran-Kumar11 <144537776+Kiran-Kumar11@users.noreply.github.com>
2023-09-19 02:59:06 +0000

    functions

---
+++ lectures/functions.js
@@ -0,0 +1,23 @@
+// 1st method
+
+function addTwoNum(num1, num2){
+    num1 = 3;
+    num2 = 5;
+    console.log(num1 + num2)
+
+}
+addTwoNum()
+
+// 2nd
+
+function kiranKumar(num1,num2){
+    console.log(num1 + num2)
+}
+kiranKumar(2,4)
+
+// 3rd
+
+function addTwo(num1, num2){
+    return num1 + num2
+}
+const result = addTwo(5,10)
--- lectures/objects-part2.js
+++ lectures/objects-part2.js
@@ -6,7 +6,18 @@
     
 // }
 
+
 const tinderUser = {};
 tinderUser.id = 123;
 tinderUser.email = "kiran@gmail.com";
 console.log(tinderUser);
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty('email'))
+
+
+const obj1 ={1:"kiran",2:"kumar"}
+const obj2 ={3:"kumar",4:"kiran"}
+const obj3 = {...obj1, ...obj2}  //spread operator
+// console.log(obj3);
+console.log(Object.assign({},obj1,obj2))
