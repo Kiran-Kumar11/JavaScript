c55d7fb2a0c5fcff104c3468101246e3b8326abd
Kiran-Kumar11 <144537776+Kiran-Kumar11@users.noreply.github.com>
2023-09-19 02:13:00 +0000

    objects part 2

---
+++ lectures/objects-part2.js
@@ -0,0 +1,12 @@
+// const newName = new Object();
+//  const newName ={}
+// console.log(newName);
+
+// const newCar(){
+    
+// }
+
+const tinderUser = {};
+tinderUser.id = 123;
+tinderUser.email = "kiran@gmail.com";
+console.log(tinderUser);
